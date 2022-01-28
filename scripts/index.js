a176a2fbdbd064bacbeeea85622e2b4ac7edbeb8
Anton Allo <qqqqton1@yandex.ru>
2022-01-29 01:57:56 +0300

    fixed bug, 2 rewiew

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const popupCloseButton = document.querySelector('.popup__close');
+const popupCloseButton = document.querySelector('.popup_close');
 const profileInfoName = document.querySelector('.profile__name');
 const profileInfoOccupation = document.querySelector('.profile__occupation');
 const popupForm = document.querySelector('.popup__form');
@@ -20,10 +20,12 @@ function closePopup() {
 profileEditButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
 
-const cardLikeButton = document.querySelector('.card__like-button');
-
-function likeButtonClicked() {
-  cardLikeButton.classList.toggle('card__like-button_active');
+function getFormProfileValue(event) {
+  event.preventDefault();
+  profileInfoName.textContent = popupFormInputName.value;
+  profileInfoOccupation.textContent = popupFormInputOccupation.value;
+  closePopup();
 }
 
-cardLikeButton.addEventListener('click', likeButtonClicked);
+popupForm.addEventListener('submit', getFormProfileValue);
+
