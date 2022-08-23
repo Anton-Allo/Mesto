e1d45f7127f732bb43fa450ef7001deb81c7000d
Anton Allo <qqqqton1@yandex.ru>
2022-08-23 20:48:43 +0300

    Final bug fix minimization css

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,12 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader'
-        }]
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+        'postcss-loader']
       }
     ]
   },
